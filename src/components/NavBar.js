a9baaa445b7e4c069761aeb1296c93b3a8b2dc14
Cuberian <themess@inbox.ru>
2021-12-24 14:31:16 +0500

    set logout function

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,21 @@
 import React, {useContext} from 'react';
 import {Context} from "../index";
-import {NavLink} from "react-router-dom";
+import {NavLink, useHistory} from "react-router-dom";
+
 import {GAMES_ROUTE, LOGIN_ROUTE, MAIN_ROUTE, NEWS_ROUTE, PROFILE_ROUTE} from "../utils/consts";
 import {observer} from "mobx-react-lite";
 
 const NavBar = observer(() => {
     const {user} = useContext(Context)
+    const history = useHistory()
+
+    const Logout = () => {
+        user.setUser({});
+        user.setIsAuth(false);
+        localStorage.removeItem('token')
+        history.push(LOGIN_ROUTE)
+    };
+
     return (
         <div className="w-full flex justify-center">
             <div className="max-w-6xl flex flex-col items-center justify-center space-y-5">
@@ -23,7 +33,12 @@ const NavBar = observer(() => {
                     <NavLink to={GAMES_ROUTE} className="px-3 py-2 bg-specialGray-600 text-white hover:bg-avocado-400 hover:text-avocado-800">Игры</NavLink>
                     <NavLink to={GAMES_ROUTE} className="px-3 py-2 bg-specialGray-600 text-white hover:bg-avocado-400 hover:text-avocado-800">Поиск</NavLink>
                     {user.isAuth ?
-                        <NavLink to={PROFILE_ROUTE} className="px-3 py-2 bg-avocado-400 text-avocado-800">Профиль</NavLink>
+                        (
+                            <React.Fragment>
+                                <NavLink to={PROFILE_ROUTE} className="px-3 py-2 bg-avocado-400 text-avocado-800">Профиль</NavLink>
+                                <button onClick={Logout} className="px-3 py-2 bg-red-400 text-avocado-800">Выйти</button>
+                            </React.Fragment>
+                        )
                     :
                         <NavLink to={LOGIN_ROUTE} className="px-3 py-2 bg-gray-500 text-white hover:bg-avocado-400 hover:text-avocado-800">Войти</NavLink>
                     }
